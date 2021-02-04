a5953367e3d5093ae75834d43ef2192185119b02
Nick Avans <nghhoese@avans.nl>
2021-02-04 19:19:01 +0100

    push it to the limit

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
+    watch: true,
     singleRun: false,
     restartOnFileChange: true
   });
